a7225685667f967f1b903b2fb20f4d0b03ad6e84
Rodrigo Cardoso <rodrigopca426281@gmail.com>
2022-07-18 21:24:40 -0300

    numerosPares
    Esse é um programa que lê um número N e o armazena em um vetor, em seguida mostra a na tela todos os números e a quantidade de todos os pares.

---
+++ arraysEColecoes/src/arrays/numerosPares.java
@@ -0,0 +1,43 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class numerosPares {
+    
+    public static void main(String[] args) {
+        
+        int i, n, quantidade;
+
+
+        Scanner tec = new Scanner(System.in);
+
+        int[] num = new int[10] ;
+
+        System.out.print("Quantos Números Você Vai Digitar? ");
+        n = tec.nextInt();
+
+        for(i = 0; i < n; i++){
+            System.out.print("Digite o "+ (i + 1)+"º  Número: ");
+            num[i] = tec.nextInt();
+        }
+        System.out.println(" ");
+        System.out.println("NÚMEROS PARES:");
+       
+        
+        quantidade = 0;
+        for(i = 0; i < n; i++ ){
+            if(num[i] % 2 == 0){
+                System.out.print(num[i] +" ");
+                quantidade++;
+            }
+        }
+
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println("QUANTIDADE DE PARES: "+ quantidade);
+        System.out.println(" ");
+
+        
+    }
+
+}
