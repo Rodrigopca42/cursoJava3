eeca1a3fa2de8317e728ee92e48c76d8397c85b4
Rodrigo Cardoso <rodrigopca426281@gmail.com>
2022-07-20 10:37:08 -0300

    DadosPessoas.java
    readme

---
+++ arraysEColecoes/src/arrays/DadoPessoas.java
@@ -0,0 +1,64 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class DadoPessoas {
+    public static void main(String[] args) {
+        
+        int i, n, posmaior, posmenor;
+        float maior, menor;
+        
+        float[] altura = new float[20];
+        String[] genero = new String[20];
+        
+        Scanner tec =  new Scanner(System.in);
+        
+        System.out.print("Quantas Pessoas Serão Digitadas? ");
+        n = tec.nextInt();
+
+        for(i = 0; i < n; i++){
+            System.out.print("Altura da "+ (i + 1)+ "° Pessoa: ");
+            altura[i] = tec.nextFloat();
+            System.out.print("Gênero da "+ (i + 1)+ "° Pessoa: ");
+            genero[i] = tec.nextLine();
+            tec.nextLine();
+        }
+        tec.close();
+
+        menor = altura[0];
+        posmenor = 0;
+        for(i = 1; i < n; i++){
+            if(altura[i] < menor){
+                menor = altura[i];
+                posmenor = i;
+            }
+        }
+        System.out.printf("MENOR ALTURA: %.2f%n", menor);
+        
+        maior = altura[0];
+        posmaior = 0;
+        for(i = 1; i < n; i++){
+            if( altura[i] > maior){
+                maior = altura[i];
+                posmaior = i;
+            }
+        }
+        System.out.printf("MAIOR ALTURA: %.2f%n",maior);
+
+        float soma = 0;
+        int nPosicao = 0;
+        for(i = 0; i < n; i++){
+            if(genero[i].equalsIgnoreCase("f")){
+                soma += altura[i];
+                nPosicao++;
+            }
+        }
+        float mediaAlturaMulher = soma / nPosicao;
+        System.out.printf("Média das Alturas das Mulheres: %.2f%n", mediaAlturaMulher);
+
+
+        int nHomens = n - nPosicao;
+        
+        System.out.printf("NÚMERO DE HOMENS: %.2f%n",nHomens);
+    }
+}
