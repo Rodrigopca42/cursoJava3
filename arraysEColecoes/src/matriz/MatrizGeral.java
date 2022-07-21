b78eac2d781760e9c59b4c78814b8d5191a4eb88
Rodrigo Cardoso <rodrigopca426281@gmail.com>
2022-07-21 19:15:06 -0300

    MatrizGeral.java
          Programa que lê uma matriz quadrada de ordem N, contendo números reais, em seguida fazer as seguintes ações:
    
          a)- Calcular e imprimir a soma de todos os elementos positivos da matriz.
    
          b)- Fazer a leitura do índice de uma linha da matriz e, daí, imprimir todos os elementos desta linha.
    
          c)- Fazer a leitura do índice de uma coluna da matriz e, daí,  imprimir todos os elementos desta coluna.
    
          d)- Imprimir os elementos da diagonal principal da matriz.
    
          e)- Alterar a matriz elevando ao quadrado todos os números negativos da mesma, em seguida imprimir a matriz alterada.

---
+++ arraysEColecoes/src/matriz/MatrizGeral.java
@@ -0,0 +1,22 @@
+package matriz;
+
+public class MatrizGeral {
+    
+
+    /*
+     * Programa que lê uma matriz quadrada de ordem N, contendo números reais, em * seguida fazer as seguites ações:
+     *  
+     * a)- Calcular e imprimir a soma de todos os elementos positivos da matriz.
+     * 
+     * b)- Fazer a leitura do índice de uma linha da matriz e, daí , imprimir
+     * todos os elementos desta linha.
+     * 
+     * c)- Fazer a leitura do índice de uma coluna da matriz e, daí,  imprimir
+     * todos os elementos desta coluna.
+     * 
+     * d)- Imprimr os elementos da diagonal principal da matriz.
+     * 
+     * e)- Alrerar a matriz elevando ao quadrado todos os números negativos da
+     * mesma, em seguida imprimir a matriz alterada.
+     */
+}
