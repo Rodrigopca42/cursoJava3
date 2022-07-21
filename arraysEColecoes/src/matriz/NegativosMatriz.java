5740ca4a0a79f04c7b4e18abb25e0104604cec93
Rodrigo Cardoso <rodrigopca426281@gmail.com>
2022-07-21 15:38:10 -0300

    readme
    readme

--- arraysEColecoes/src/matriz/NegativosMatriz.java
+++ arraysEColecoes/src/matriz/NegativosMatriz.java
@@ -1,5 +1,38 @@
 package matriz;
 
+import java.util.Scanner;
+
 public class NegativosMatriz {
     
+    public static void main(String[] args) {
+        
+        int m, n, i, j;
+
+        int[][] mat = new int[10][10];
+        
+        Scanner tec = new Scanner(System.in);
+
+        System.out.print("Digite a quantidade de linhas da matriz: ");
+        m = tec.nextInt();
+
+        System.out.print("Digite a quantidade de colunas da matriz: ");
+        n = tec.nextInt();
+
+        for(i = 0;i < m; i++){
+            for(j = 0; j < n; j++){
+                System.out.print("Elemento ["+i+", "+j+"]: ");
+                mat[i][j] = tec.nextInt();
+            }
+        }
+        tec.close();
+
+        System.out.println("Valores Negativos:");
+        for(i = 0; i < m; i++){
+            for(j = 0; j < n; j++){
+                if(mat[i][j] < 0){
+                   System.out.println(mat[i][j]);
+                }
+            }
+           }
+    }
 }
